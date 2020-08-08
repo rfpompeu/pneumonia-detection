fadc8f27183099b4c8500ab901c08685e7ede2e0
rafa <rfpompeup@gmail.com>
2020-08-08 20:27:40 -0300

    confusion matrix

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# pneumonia-detection
+# Pneumonia Detection
+
+## SVM
+### SVC
+| Option | Acurracy|
+| --- | --- |
+| Kernel="rbf", C=1 | 0.683 |
+#### Confusion Matrix
+### NuSVC
+### LinearSVC
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import Data.data as dt
+
+import pandas as pd
+
 from sklearn.cluster import KMeans
-import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 import Classification_Algorithms.SVM.svc as svc
+
 
 descriptors = dt.list_descriptors("train")
 kmeans = KMeans(n_clusters=5).fit(descriptors)
@@ -10,4 +19,17 @@ dictionary = kmeans.cluster_centers_
 x_train, y_train = dt.labels("train", dictionary)
 x_test, y_test = dt.labels("test", dictionary)
 
-print(svc.acurracy(x_train, x_test, y_train, y_test))
+prevision = svc.prevision(x_train, x_test, y_train, y_test)
+print(accuracy_score(y_test, prevision))
+
+
+def confusion_mat():
+    matrix = confusion_matrix(y_test, prevision)
+    matrix = (matrix/sum(matrix)).T
+    matrix = pd.DataFrame(matrix, columns=['Normal', 'Pneumonia'], index=[
+                          'Normal', 'Pneumonia'])
+    sns.heatmap(matrix, annot=True, vmin=0, vmax=1, cmap="YlGnBu")
+    plt.show()
+
+
+confusion_mat()
